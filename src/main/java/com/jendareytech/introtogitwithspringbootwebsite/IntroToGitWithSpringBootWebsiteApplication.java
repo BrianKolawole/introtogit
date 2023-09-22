d0631f62308670a7cb0b1d4955932f3f1f0bf9f8
BrianKolawole <dammy.brian@gmail.com>
2023-09-21 23:10:40 -0500

    first commit

---
+++ src/main/java/com/jendareytech/introtogitwithspringbootwebsite/IntroToGitWithSpringBootWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.jendareytech.introtogitwithspringbootwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToGitWithSpringBootWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToGitWithSpringBootWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jendareytech/introtogitwithspringbootwebsite/IntroToGitWithSpringBootWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jendareytech.introtogitwithspringbootwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroToGitWithSpringBootWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
